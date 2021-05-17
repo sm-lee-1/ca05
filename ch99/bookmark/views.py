64019008fcd1d407cb98ccda5fd109af8908b196
sm-lee-1 <eelevilo@gmail.com>
2021-05-17 21:51:05 +0900

    Initial commit

---
+++ ch99/bookmark/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from bookmark.models import Bookmark
+from django.views.generic import ListView, DetailView
+
+# Create your views here.
+class BookmarkLV(ListView):  # 테이블의 레코드 리스트를 보여주기 위한 뷰
+    model = Bookmark
+
+class BookmarkDV(DetailView): # 테이블의 특정 레코드에 대한 상세 정보를 보여주기 위한 뷰
+    model = Bookmark
